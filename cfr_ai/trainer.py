5fc874e5488f64cd000313fa5971c252495c3282
Maciej Pomykała <hobixpl@gmail.com>
2022-01-20 21:45:48 +0000

    Improve regret-based pruning

--- cfr_ai/trainer.py
+++ cfr_ai/trainer.py
@@ -10,7 +10,7 @@ class Trainer():
         self.BlefCards = BlefCards
         self.NumCards = Params.NumCards
 
-    def get_node_value(self, hands: List[List[str]], history: List[int], reach_probability: float, active_player: int, mc_player: int, warm_up: bool, prune_feast: bool):
+    def get_node_value(self, hands: List[List[str]], history: List[int], reach_probability: float, active_player: int, mc_player: int, warm_up: bool, prune_feast: int):
         if Game.check_finish(history):
             return Game.get_payoff(history, hands)
         hand = hands[active_player]
@@ -25,17 +25,13 @@ class Trainer():
 
         if active_player == mc_player:
             strategy = info_set.get_strategy(reach_probability, warm_up)
-            for i in range(info_set.num_actions):
-                if info_set.regrets[i] >= -1000 or prune_feast:
-                    counterfactual_values[i] = -self.get_node_value(hands, history + [info_set.possible_actions[i]], reach_probability * strategy[i], opponent, mc_player, warm_up, prune_feast)
-                else:
-                    counterfactual_values[i] = 0
+            for i, action in enumerate(info_set.possible_actions):
+                if info_set.regrets[i] >= -200 or (prune_feast == 1 and info_set.regrets[i] >= -500) or prune_feast == 2:
+                    counterfactual_values[i] = -self.get_node_value(hands, history + [action], reach_probability * strategy[i], opponent, mc_player, warm_up, prune_feast)
             node_value = counterfactual_values.dot(strategy)
             for i in range(info_set.num_actions):
-                if info_set.regrets[i] >= -1000:
+                if info_set.regrets[i] >= -200 or (prune_feast == 1 and info_set.regrets[i] >= -500) or prune_feast == 2:
                     info_set.regrets[i] += counterfactual_values[i] - node_value
-                elif prune_feast:
-                    info_set.regrets[i] += 10 * (counterfactual_values[i] - node_value)
         else:
             strategy = info_set.get_strategy(1.0, warm_up)
             action = random.choices(info_set.possible_actions, weights=strategy, k=1)[0]
@@ -47,7 +43,9 @@ class Trainer():
         for i in range(num_iterations):
             report_progress(i, num_iterations)
             warm_up = i < num_iterations * 0.3
-            prune_feast = i % 10 == 0
+            prune_feast = 0
+            if (i % 5 == 0): prune_feast = 1
+            if (i % 20 == 0): prune_feast = 2
             for mc_player in range(2):
                 hands = Game.deal_cards(self.BlefCards, self.NumCards)
                 util += self.get_node_value(hands, [], 1.0, 0, mc_player, warm_up, prune_feast)
