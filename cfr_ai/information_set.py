cd74bfde5818101df86490918ce6e2be580d22ba
Maciej Pomykała <hobixpl@gmail.com>
2022-01-22 02:15:51 +0000

    Don't skip four of a kind with 5 cards

--- cfr_ai/information_set.py
+++ cfr_ai/information_set.py
@@ -9,8 +9,8 @@ def get_relevant_actions(NumCards: List[int]):
         return [a for a in range(12)] + [a for a in range(30, 36)] + [88]
     elif sum(NumCards) <= 4:
         return [a for a in range(27)] + [a for a in range(30, 36)] + [88] # Skipped four of a kind
-    elif sum(NumCards) <= 5:
-        return [a for a in range(29)] + [a for a in range(30, 70)] + [88] # Skipped from four of a kind
+    elif sum(NumCards) <= 6:
+        return [a for a in range(29)] + [a for a in range(30, 66)] + [a for a in range(70, 76)] + [88] # Skipped flush and straight flush
     elif sum(NumCards) <= 13:
         return [a for a in range(0, 70)] + [88] # Skipped from four of a kind
     else:
