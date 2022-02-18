6f7244365b1ca1b787a878ea3ca220d1db8957b4
Maciej Pomykała <hobixpl@gmail.com>
2022-02-18 12:55:04 +0000

    Refactor inputs to get_exploitability

--- cfr_ai/training.py
+++ cfr_ai/training.py
@@ -17,17 +17,18 @@ cfr_trainer = Trainer(BlefCards, Params)
 util0, util1 = cfr_trainer.train(args.num_iterations)
 print(f"Expected values, depenging on who starts, are {util0:.3f} and {util1:.3f}")
 
+cfr_strategy = {k: v.get_final_strategy() for k,v in cfr_trainer.infoset_map.items()}
+
 with open('cfr_ai/outputs/' + "_".join(str(x) for x in args.NumCards) + '.csv', 'w', newline="") as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames=['k', 'v'])
     csv_row = writer.writeheader()
-    for k,v in cfr_trainer.infoset_map.items():
-        strategy = v.get_final_strategy()
-        if strategy[-1] < 1.0:
-            csv_row = writer.writerow({"k": k, "v": encode_probabilities(strategy)})
+    for k,v in cfr_strategy:
+        if v[-1] < 1.0:
+            csv_row = writer.writerow({"k": k, "v": encode_probabilities(v)})
 
 print(f"\nComputing exploitability")
 start_time = datetime.now()
-print(f"\nExploitability when player 0 starts: " + str(get_exploitability(cfr_trainer.infoset_map, BlefCards, Params, 0)))
+print(f"\nExploitability when player 0 starts: " + str(get_exploitability(cfr_strategy, BlefCards, Params, 0)))
 if Params.NumCards[0] != Params.NumCards[1]:
-    print(f"\nExploitability when player 1 starts: " + str(get_exploitability(cfr_trainer.infoset_map, BlefCards, Params, 1)))
+    print(f"\nExploitability when player 1 starts: " + str(get_exploitability(cfr_strategy, BlefCards, Params, 1)))
 print("Time spent: ", datetime.now() - start_time)
