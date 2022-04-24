233a00bab62a43dab43a7e79978727461d1e76eb
Maciej Pomykała <hobixpl@gmail.com>
2022-04-24 02:38:02 +0100

    Adjust agent to be the worker

--- cfr_ai/agent.py
+++ cfr_ai/agent.py
@@ -1,7 +1,5 @@
 import random
 import csv
-from os import path
-from conservative_crawling_ai.agent import determine_action as ask_porevit
 from cfr_ai.encoding import decode_probabilities
 from cfr_ai.information_set import make_key, get_possible_actions, get_hand_abstraction
 
@@ -10,30 +8,24 @@ def determine_action(game_state):
     players = game_state.get("players", [])
     hand_sizes = [player.get("n_cards") for player in players]
     hand_sizes.sort()
-    directory = 'cfr_ai/outputs/' + "_".join(str(x) for x in hand_sizes)
-    if not path.exists(directory):
-        print("Asking Porevit")
-        sampled_action = ask_porevit(game_state)
+    history = []
+    if game_state.get("history"):
+        history = [action["action_id"] for action in game_state.get("history")]
+    matching_hands = [hand for hand in game_state.get("hands", []) if hand.get("nickname") == agent_nickname]
+    my_cards = [str(card["value"]) + str(card["colour"]) for card in matching_hands[0]["hand"]]
+    hand_abstraction = get_hand_abstraction(my_cards, hand_sizes)
+    key = make_key(my_cards, hand_abstraction, history)
+    split_key = key.split('-')
+    filename = 'cfr_ai/outputs/' + "_".join(str(x) for x in hand_sizes) + '/' + split_key[0] + '/' + split_key[1] + '.csv'
+    relevant_actions = get_possible_actions(history, hand_sizes)
+    with open(filename, 'r', encoding='utf-8') as f:
+        strategy_list = csv.reader(f)
+        matching_strategies = [x[1] for x in strategy_list if x[0] == '-'.join(split_key[2:])]
+    if len(matching_strategies) == 1:
+        strategy = decode_probabilities(matching_strategies[0])
+        return random.choices(relevant_actions, weights=strategy, k=1)[0]
+    elif len(history) == 0:
+        print("No policy found though the round has just begun. Betting high card 9")
+        return 0
     else:
-        history = []
-        if game_state.get("history"):
-            history = [action["action_id"] for action in game_state.get("history")]
-        matching_hands = [hand for hand in game_state.get("hands", []) if hand.get("nickname") == agent_nickname]
-        my_cards = [str(card["value"]) + str(card["colour"]) for card in matching_hands[0]["hand"]]
-        hand_abstraction = get_hand_abstraction(my_cards, hand_sizes)
-        key = make_key(my_cards, hand_abstraction, history)
-        split_key = key.split('-')
-        filename = 'cfr_ai/outputs/' + "_".join(str(x) for x in hand_sizes) + '/' + split_key[0] + '/' + split_key[1] + '.csv'
-        relevant_actions = get_possible_actions(history, hand_sizes)
-        with open(filename, 'r', encoding='utf-8') as f:
-            strategy_list = csv.reader(f)
-            matching_strategies = [x[1] for x in strategy_list if x[0] == '-'.join(split_key[2:])]
-        if len(matching_strategies) == 1:
-            strategy = decode_probabilities(matching_strategies[0])
-            sampled_action = random.choices(relevant_actions, weights=strategy, k=1)[0]
-        elif len(history) == 0:
-            print("Asking Porevit")
-            sampled_action = ask_porevit(game_state)
-        else:
-            sampled_action = 88
-    return sampled_action
+        return 88
