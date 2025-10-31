f88f29e5f0955ce695c1611aafe0d8309059966b
Adrian Golian <adix9512@gmail.com>
2025-10-31 10:57:31 +0100

    Set hidden layer width to 128

--- nfsp_ai/nfsp_run_local.py
+++ nfsp_ai/nfsp_run_local.py
@@ -1396,7 +1396,7 @@ def main():
             n_step=6,
             burst_rl_updates_on_reward=4,
             burst_reward_threshold=0.5,
-            hidden=256
+            hidden=128
         ),
     )
 
